a8365880657caaee68c7e8ad6cc30a3481d4c7de
Amit Erez <amiterez98@gmail.com>
2020-04-07 01:55:50 +0300

    add default image

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -8,15 +8,28 @@ const RecipesList = ({ recipesList, baseUrl, onRouteChange, setRecipeId }) => {
 	<div className='container' style={{display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', gridGap:'20px', margin: '20px'}}>
 		{
 			recipesList.map((recipe, i) => {
-				return (
-					<RecipeCard 
-					title={recipe.title} 
-					key={i}
-					imageUrl={baseUrl + recipe.image}
-					recipeId={recipe.id}
-					onRouteChange={onRouteChange}
-					setRecipeId={setRecipeId}/>
-				)
+				//if no image was uploaded - set default image
+				if (recipe.image === 'no file uploaded') {
+					return (
+						<RecipeCard 
+						title={recipe.title} 
+						key={i}
+						imageUrl=''
+						recipeId={recipe.id}
+						onRouteChange={onRouteChange}
+						setRecipeId={setRecipeId}/>
+					)
+				} else {
+					return (
+						<RecipeCard 
+						title={recipe.title} 
+						key={i}
+						imageUrl={baseUrl + recipe.image}
+						recipeId={recipe.id}
+						onRouteChange={onRouteChange}
+						setRecipeId={setRecipeId}/>
+					)
+				}
 			})
 		}
 	</div>
