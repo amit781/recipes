f1607d942c7dfe4f8603eceb1b1b1ad8a8aaee05
Amit Erez <amiterez98@gmail.com>
2020-04-08 23:33:10 +0300

    change recipe card to react router

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import defaultImage from "./default.jpg";
 
-const RecipeCard = ({ title, imageUrl, onRouteChange, setRecipeId, recipeId }) => {
-	const onClick = () => {
-		onRouteChange('recipe-page');
-		setRecipeId(recipeId);
+
+class RecipeCard extends Component {
+	constructor() {
+		super();
+		this.state = {
+			toRcipePage: false
+		}
+	};
+
+	onClick = () => {
+		this.setState({toRcipePage: true});
+		this.props.setRecipeId(this.props.recipeId);
 	}
-	return (
-		<article className="pa2" width='200px'>
-			<img src={imageUrl} alt='recipe' width='200px' height='200px' style={{objectFit: 'cover'}} className="pointer" onClick={onClick} />
-            <div>
-                <h3 className="pointer" onClick={onClick}>{title}</h3>
-            </div>
-	    </article>
-	)
+
+	render() {
+		const { title, imageUrl, onRouteChange, setRecipeId, recipeId } = this.props;
+		if (this.state.toRcipePage) {
+			return <Redirect to="/recipePage"/>
+		}
+		return (
+			<article className="pa2" width='200px'>
+				<img style={{objectFit: 'cover'}} src={imageUrl} alt='recipe' width='200px' height='200px' className="pointer" onClick={this.onClick} />
+	            <div>
+	                <h3 className="pointer" onClick={this.onClick}>{title}</h3>
+	            </div>
+		    </article>
+		)
+	}
 }
+
+// const RecipeCard = ({ title, imageUrl, onRouteChange, setRecipeId, recipeId }) => {
+// 	const onClick = () => {
+// 		onRouteChange('recipe-page');
+// 		setRecipeId(recipeId);
+// 	}
+// 	return (
+// 		<article className="pa2" width='200px'>
+// 			<img src={imageUrl} alt='recipe' width='200px' height='200px' style={{objectFit: 'cover'}} className="pointer" onClick={onClick} />
+//             <div>
+//                 <h3 className="pointer" onClick={onClick}>{title}</h3>
+//             </div>
+// 	    </article>
+// 	)
+// }
 
 export default RecipeCard;
